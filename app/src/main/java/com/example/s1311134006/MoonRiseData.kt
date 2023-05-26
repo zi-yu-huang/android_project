be4eed1cd8238bfc5b4d26014d279ca804c22ea6
zi-yu-huang <ziyuhuang1007@gmail.com>
2023-05-26 16:06:34 +0800

    moon

--- app/src/main/java/com/example/s1311134006/MoonRiseData.kt
+++ app/src/main/java/com/example/s1311134006/MoonRiseData.kt
@@ -1,54 +1,55 @@
 package com.example.s1311134006
+import com.fasterxml.jackson.annotation.JsonProperty
 
-//data class MoonRiseData (
-//    val success: String,
-//    val result: Result,
-//    val records: Records,
-//){
-//    data class Result(
-//        @JsonProperty("resource_id")
-//        val resourceId: String,
-//        val fields: List<Field>,
-//    ){
-//        data class Field(
-//            val id: String,
-//            val type: String,
-//        )
-//    }
-//    data class Records(
-//        val dataid: String,
-//        val note: String,
-//        val locations: Locations,
-//    )
-//    data class Locations(
-//        val location: List<Location>,
-//    ){
-//        data class Location(
-//            val time: List<Time>,
-//            @JsonProperty("CountyName")
-//            val countyName: String,
-//        ){
-//            data class Time(
-//                @JsonProperty("Date")
-//                val date: String,
-//                @JsonProperty("BeginCivilTwilightTime")
-//                val beginCivilTwilightTime: String,
-//                @JsonProperty("SunRiseTime")
-//                val sunRiseTime: String,
-//                @JsonProperty("SunRiseAZ")
-//                val sunRiseAz: String,
-//                @JsonProperty("SunTransitTime")
-//                val sunTransitTime: String,
-//                @JsonProperty("SunTransitAlt")
-//                val sunTransitAlt: String,
-//                @JsonProperty("SunSetTime")
-//                val sunSetTime: String,
-//                @JsonProperty("SunSetAZ")
-//                val sunSetAz: String,
-//                @JsonProperty("EndCivilTwilightTime")
-//                val endCivilTwilightTime: String,
-//            )
-//
-//        }
-//    }
-//}
+class MoonRiseData (
+    val success: String,
+    val result: Result,
+    val records: Records,
+){
+    data class Result(
+        @JsonProperty("resource_id")
+        val resource_id: String,
+        val fields: List<Field>,
+    ){
+        data class Field(
+            val id: String,
+            val type: String,
+        )
+    }
+    data class Records(
+        val dataid: String,
+        val note: String,
+        val locations: Locations,
+    )
+    data class Locations(
+        val location: List<Location>,
+    ){
+        data class Location(
+            val time: List<Time>,
+            @JsonProperty("CountyName")
+            val CountyName: String,
+        ){
+            data class Time(
+                @JsonProperty("Date")
+                val Date: String,
+                @JsonProperty("BeginCivilTwilightTime")
+                val BeginCivilTwilightTime: String,
+                @JsonProperty("SunRiseTime")
+                val SunRiseTime: String,
+                @JsonProperty("SunRiseAZ")
+                val SunRiseAZ: String,
+                @JsonProperty("SunTransitTime")
+                val SunTransitTime: String,
+                @JsonProperty("SunTransitAlt")
+                val SunTransitAlt: String,
+                @JsonProperty("SunSetTime")
+                val SunSetTime: String,
+                @JsonProperty("SunSetAZ")
+                val SunSetAZ: String,
+                @JsonProperty("EndCivilTwilightTime")
+                val EndCivilTwilightTime: String,
+            )
+
+        }
+    }
+}
