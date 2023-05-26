3b19c515c7df5c06dead7de4e07c386ec8bbf57f
zi-yu-huang <ziyuhuang1007@gmail.com>
2023-05-26 16:03:47 +0800

    sunrise

--- app/src/main/java/com/example/s1311134006/SunRiseActivity.kt
+++ app/src/main/java/com/example/s1311134006/SunRiseActivity.kt
@@ -9,16 +9,18 @@ import android.widget.ImageView
 import android.widget.TextView
 import com.example.s1311134006.databinding.ActivityMainBinding
 import com.example.s1311134006.databinding.ActivitySunRiseBinding
+import org.w3c.dom.Text
 import retrofit2.Call
 import retrofit2.Response
 import java.time.LocalDateTime
+
 class SunRiseActivity : AppCompatActivity() {
     private val TAG = "LogDemo"
-    private lateinit var binding:ActivitySunRiseBinding
-    private lateinit var arrow:ImageView
-    private lateinit var homeArrow:ImageView
-    private lateinit var sunRiseTime:TextView
-    private lateinit var sunSetTime:TextView
+    private lateinit var binding: ActivitySunRiseBinding
+    private lateinit var arrow: ImageView
+    private lateinit var homeArrow: ImageView
+    private lateinit var sunRiseTime: TextView
+    private lateinit var sunSetTime: TextView
     private var sunriseList: MutableList<sunriseData.Records.Locations.Location.Time> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,45 +33,55 @@ class SunRiseActivity : AppCompatActivity() {
         sunRiseTime = binding.sunriseTime
         sunSetTime = binding.sunsetTime
         arrow.setOnClickListener({
-            startActivity(Intent(this,moonRiseActivity::class.java))
+            startActivity(Intent(this, moonRiseActivity::class.java))
         })
         homeArrow.setOnClickListener({
-            startActivity(Intent(this,MainActivity::class.java))
+            startActivity(Intent(this, MainActivity::class.java))
         })
     }
-    private fun getData(){
+
+    private fun getData() {
         val currentDateTime = LocalDateTime.now()
         sunriseApi
             .retrofitService
-            .getData(Authorization="CWB-F720A7AF-8FFF-4EBB-AFEA-0B3A675FF9CC",format="JSON")
-            .enqueue(object:retrofit2.Callback<sunriseData>{
-                override fun onFailure(call: Call<sunriseData>, t:Throwable){
-                    Log.e("fail",t.toString())
+            .getData(Authorization = "CWB-F720A7AF-8FFF-4EBB-AFEA-0B3A675FF9CC", format = "JSON")
+            .enqueue(object : retrofit2.Callback<sunriseData> {
+                override fun onFailure(call: Call<sunriseData>, t: Throwable) {
+                    Log.e("fail", t.toString())
                 }
+
                 override fun onResponse(
                     call: Call<sunriseData>,
                     response: Response<sunriseData>
                 ) {
                     Log.d(TAG, response.raw().toString())
-                    Log.e("saasdasdasd", "dsfsdfsd")
                     if (response.isSuccessful) {
-//                        Log.d(TAG,response.body()?.success.toString())
-//                        Log.d(TAG,response.body()?.records.toString())
-//                        Log.d(TAG,response.body()?.result.toString())
+
                         response.body()?.records?.locations?.location?.forEach {
-                            Log.d("api", it.toString())
+//                            Log.d("api", it.toString())
                             if (it.CountyName == "臺中市") {
-                                Log.d(TAG,"jkj")
-//                                sunriseList.add(it)
-//                                setUVData()
-//                                setUVString()
+                                it.time.forEach { element ->
+                                    val currentDateTimeString = currentDateTime.toString().split("T")[0]
+                                    if (element.Date == currentDateTimeString) {
+                                        sunriseList.add(element)
+                                        setSunText()
+                                    }
+
+                                }
                             }
+
                         }
                     }
 
 
-//                    Log.e(TAG,sunriseList.toString())
+                    Log.e(TAG, sunriseList.toString())
                 }
             })
+    }
+    private fun setSunText(){
+        var sunriseTime:TextView=findViewById(R.id.sunriseTime)
+        var sunsetTime:TextView=findViewById(R.id.sunsetTime)
+        sunriseTime.text = sunriseList[0].SunRiseTime
+        sunsetTime.text = sunriseList[0].SunSetTime
     }
 }
